5d9f2511636532af71b96a6f2bd9e4363d2fcaec
Sam Baraka <barakaarunga@gmail.com>
2025-09-07 22:12:52 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+
+// Initial employee object
+const employee = {
+  name: "Sam"
+};
+
+//  Non-destructive update
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value
+  };
+}
+
+//Destructive update
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+//Non-destructive delete
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+//Destructive delete
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
